dd4a047105cacf5acd7bb0a75221034788aa82a4
davidsmfreire <davidsmfreire@gmail.com>
2024-10-05 11:25:03 +0100

    feat: accept spotify links

--- listeny.py
+++ listeny.py
@@ -1,9 +1,12 @@
+import json
 import os
 from typing import List, NamedTuple
+from bs4 import BeautifulSoup
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import requests
 from youtube_search import YoutubeSearch
 from yt_dlp import YoutubeDL
 
@@ -64,6 +67,35 @@ async def play_next_in_queue(ctx):
         await ctx.send(f"Now playing: {song.title}.{queue_repr}")
 
 
+def get_song_title_from_spotify_url(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        raise Exception(f"Failed to retrieve page. Status code: {response.status_code}")
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    redirect = soup.find('script', {'id': 'urlSchemeConfig'})
+    if redirect is not None:
+        redirect_content: dict = json.loads(redirect.string)
+        if url := redirect_content.get("redirectUrl"):
+            return get_song_title_from_spotify_url(url)
+
+    meta_title = soup.find('meta', property='og:title')
+    meta_artist = soup.find('meta', property='og:description')
+
+    if not meta_title or not meta_artist:
+        raise Exception("Failed to find artist and title tags")
+
+    song_title = meta_title['content']
+    artist_name = meta_artist['content'].split(" · ")[0]
+    return song_title, artist_name
+
+
 @bot.command(name="queue", help="View queue")
 async def view_queue(ctx):
     if len(music_queue) > 0:
@@ -83,10 +115,17 @@ async def play_media(ctx, *, search_query: str):
 
         voice_client = ctx.voice_client
 
-        if "https://" in search_query:
+        if "https://" in search_query and "spotify" not in search_query:
             url = search_query
             title = None
         else:
+            if "https://" in search_query and "spotify" in search_query:
+                try:
+                    title, artist = get_song_title_from_spotify_url(search_query)
+                except Exception as e:
+                    await ctx.send(f"Failed to get song from spotify url: {str(e)}")
+                    return
+                search_query = f"{title}, {artist}"
             results = YoutubeSearch(search_query, max_results=1).to_dict()
             title = results[0]["title"]
             url = "https://youtube.com" + results[0]["url_suffix"]
@@ -130,4 +169,3 @@ bot.run(TOKEN)
 
 
 # TODO: pause/resume (hard?)
-# TODO: accept spotify urls
