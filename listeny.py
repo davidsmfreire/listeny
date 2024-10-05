a23b8bf2cf55ce502465b183ddf97229b13f4810
davidsmfreire <davidsmfreire@gmail.com>
2024-10-05 14:32:56 +0100

    feat: ffmpeg http resiliency, playnow command and track current song for future features

--- listeny.py
+++ listeny.py
@@ -1,6 +1,7 @@
 import json
 import os
-from typing import List, NamedTuple
+import time
+from typing import List, NamedTuple, Optional
 from bs4 import BeautifulSoup
 
 import discord
@@ -12,7 +13,7 @@ from yt_dlp import YoutubeDL
 
 load_dotenv()
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.environ["DISCORD_TOKEN"]
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -28,26 +29,32 @@ youtube_dl_opts = {
     "extract_flat": "True",
 }
 
+ffmpeg_before_options = "-reconnect 1 -reconnect_on_network_error 1"
+
 class Song(NamedTuple):
     title: str
-    audio: discord.FFmpegPCMAudio
+    url: str
 
 
 QUEUE_LIMIT = 30
 music_queue: List[Song] = []
+current_song: Optional[Song] = None
+current_song_start_time: Optional[int] = None
 
+def seconds_to_hms(seconds: int):
+    return time.strftime('%H:%M:%S', time.gmtime(seconds))
 
 def get_queue_repr():
     return "\n".join([f"{i} - {m.title}" for i,m in enumerate(music_queue, 1)])
 
 
 def get_play_next_callback(ctx):
-    def play_next_callback(e):
+    def play_next_callback(error):
         bot.loop.create_task(play_next_in_queue(ctx))
     return play_next_callback
 
 
-async def add_to_queue(ctx, song):
+async def add_to_queue(ctx, song: Song):
     if len(music_queue) >= QUEUE_LIMIT:
         await ctx.send("Can't add more songs to queue, limit reached (30)")
     music_queue.append(song)
@@ -60,7 +67,7 @@ async def play_next_in_queue(ctx):
         if voice_client.is_playing():
             voice_client.stop()
         song: Song = music_queue.pop(0)
-        voice_client.play(song.audio, after=get_play_next_callback(ctx))
+        await play_song(ctx, song)
         queue_repr = ""
         if len(music_queue) > 0:
             queue_repr = "\nQueue:\n" + get_queue_repr()
@@ -104,9 +111,25 @@ async def view_queue(ctx):
         await ctx.send("Queue is empty")
 
 
-# Command to join voice channel and play music
-@bot.command(name="play", help="Play music from YouTube")
-async def play_media(ctx, *, search_query: str):
+async def play_song(ctx, song: Song, offset: int = 0):
+    ctx.voice_client.stop()
+
+    global current_song, current_song_start_time
+
+    before_options = ffmpeg_before_options
+    if offset > 0:
+        before_options = ffmpeg_before_options + f"-ss {seconds_to_hms(offset)}"
+
+    audio = discord.FFmpegPCMAudio(song.url, before_options=before_options)
+    ctx.voice_client.play(
+        audio,
+        after=get_play_next_callback(ctx),
+    )
+    current_song = song
+    current_song_start_time = int(time.time())
+
+
+async def _play_media(ctx, search_query: str, now: bool):
     if ctx.author.voice:
         voice_channel = ctx.author.voice.channel
 
@@ -135,18 +158,29 @@ async def play_media(ctx, *, search_query: str):
             if title is None:
                 title = song_info["title"]
 
-        audio_source = discord.FFmpegPCMAudio(song_info["url"])
+        song = Song(title=title, url=song_info["url"])
 
-        if not voice_client.is_playing():
-            voice_client.play(
-                audio_source,
-                after=get_play_next_callback(ctx),
-            )
+        if now or not voice_client.is_playing():
+            await play_song(ctx, song)
             await ctx.send(f"Now playing: {title}")
         else:
-            await add_to_queue(ctx, song=Song(title=title, audio=audio_source))
+            await add_to_queue(ctx, song)
     else:
         await ctx.send("You need to be in a voice channel to play music!")
+
+
+# Command to join voice channel and play music
+@bot.command(name="play", help="Play music from YouTube")
+async def play_media(ctx, *, search_query: str):
+    await _play_media(ctx, search_query, False)
+
+
+@bot.command(name="playnow", help="Play music from YouTube without queue")
+async def play_media_now(ctx, *, search_query: str):
+    if not ctx.author.guild_permissions.administrator:
+        await ctx.send("Only administrators can use this command!")
+        return
+    await _play_media(ctx, search_query, True)
 
 
 @bot.command(name="skip", help="Skip music from queue")
